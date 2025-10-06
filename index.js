0cbb36104075f87aeab984c3ad9bfc73248b355d
Дарья Сидорова <sidorovadas@gmail.com>
2025-10-06 23:06:23 +0300

    Самое последнее изменение

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 //Простые данные. В переменной хранится возраст пользователя. Ему может быть 0 лет.
 let userAge = 0;
-console.log(userAge)
+console.log(userAge);
 //Флаги состояния. Задача: Нужно отслеживать, авторизован ли пользователь на сайте.
 isUserAuthorized = true;
-console.log(isUserAuthorized)
+console.log(isUserAuthorized);
 //Коллекции элементов. Задача: В переменной хранится список всех товаров в корзине интернет-магазина
-const allProducts = ['banans', "apples", "lemons"]
-console.log(allProducts)
+const allProducts = ["banans", "apples", "lemons"];
+console.log(allProducts);
 //Сложные данные (объект). Задача: В переменной хранится информация о книге: её название, автор, год издания и количество страниц.
 bookOfTheDay = {
-nameOfBook: "Унесенные ветром",
-author: "Маргарет Митчел",
-yearOfPublication: 1936,
-numberOfPage: 1088
-}
-console.log(bookOfTheDay)
+  nameOfBook: "Унесенные ветром",
+  author: "Маргарет Митчел",
+  yearOfPublication: 1936,
+  numberOfPage: 1088,
+};
+console.log(bookOfTheDay);
 
 //Вычисляемые значения. Задача: В переменную записывается общая сумма всех товаров в корзине после применения всех скидок. Как назвать переменную?
-const sumAfterDiscount 
-const sumAll //сумма за заказ
-const sumDiscount //сумма всех скидок
+const sumAfterDiscount = 50;
+const sumAll = 80; //сумма за заказ
+const sumDiscount = 30; //сумма всех скидок
 
 //События и таймеры. Задача: В переменной хранится ID таймера, который запускает какое-то действие каждые 5 секунд. Эта переменная нужна, чтобы позже можно было этот таймер остановить.
-const CircleTimer = document.getElementById("time_count")
+const CircleTimer = document.getElementById("time_count");
 
 //Элементы DOM. Задача: Нужно работать с кнопкой в интерфейсе, при нажатии на которую открывается модальное окно. Как назвать переменную, которая ссылается на эту HTML-кнопку?
 //  <button class='open_modal'>Регистрация</button> // та самая кнопка
-const registrationOpenForm = document.getElementsByClassName('open_modal');
+const registrationOpenForm = document.getElementsByClassName("open_modal");
 
 // Булевы значения (да/нет). Задача: Переменная отвечает на вопрос: "Отображен ли сейчас этот попап на экране?" Как назвать переменную?
 const isPopOnDisplay = true;
 //В переменной собирается приветственное сообщение для пользователя, например, "Добро пожаловать, [Имя]!". Как назвать переменную?
 let SayHello = "userName";
 //Функции-обработчики* Задача: Создаётся функция, которая будет вызвана при отправке пользователем формы регистрации. Как назвать эту функцию?
-function registrationFormSubmit (){alert("Вы зарегистрированы")};
-
-
+function registrationFormSubmit() {
+  alert("Вы зарегистрированы");
+}
