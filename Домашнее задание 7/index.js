ed0e101cad2a461377decf534f5ea1e5ae99d531
Дарья Сидорова <sidorovadas@gmail.com>
2025-11-16 22:48:29 +0300

    Добавлены все решения по домашнему заданию

--- Домашнее задание 7/index.js
+++ Домашнее задание 7/index.js
@@ -31,6 +31,20 @@
 // массив, содержащий пары чисел, которые в сумме должны
 // быть равны семи: (0:7), (1:6) и т.д
 
+// const arrNum = [1, 2, 3, 4, 5, 6, 7, -2, 9, 0];
+// function findPairs(arrNum) {
+//     let pairs = [];
+//     for (let i=0; i < arrNum.length; i++) {
+//         for (let h=i+1; h<arrNum.length; h++) {
+//             if (arrNum[i] + arrNum[h]===7) {
+//                 pairs.push([arrNum[i], arrNum[h]]);
+//             }
+//         }
+//     }
+//     return pairs;
+// }
+// const sumPairs= findPairs(arrNum);
+// console.log(sumPairs);
 
 
 
@@ -86,12 +100,76 @@
 // const arr3=[15,19,27];
 
 // function compilateArr(arr1, arr2, arr3) {
-//    let newArr= arr1.concat(arr2, arr3).sort((a, b) => (a-b));
-//     let strComma = newArr.join(" ") 
-//     let strWioutComma= strComma.replace(/,/g, " ");
-//     {
-//     return strWioutComma
+//    let newArr= arr1.concat(arr2, arr3).sort((a, b) => (b-a)); 
+//     { 
+//     return newArr;
 //     }
 // }
 // const compArr=compilateArr(arr1, arr2, arr3);
-// console.log(compArr.split(" ").map(Number));
+//  console.log(compArr);
+
+// 8. Дан двухмерный массив с числами, например [[1, 2, 3], [4, 5],
+// [6]]. Найдите сумму элементов этого массива. Массив, конечно
+// же, может быть произвольным.
+
+// const twoDimensionArry = [[1, 10, 15], [11, 7, 4]];
+// function arraySum(twoDimensionArry) {
+//     newArr=twoDimensionArry.flat(Infinity) 
+//      let sum=newArr.reduce((accum, num) => accum + num, 0)
+//      return sum;
+// }
+// const arrSum=arraySum(twoDimensionArry);
+// console.log(arrSum);
+
+// 9. Дан массив с числами. Не используя метода reverse
+// переверните его элементы в обратном порядке.
+
+// const arrMas = [1, 2, 3, 4, 5];
+// function toReverseArr(arrMas) {
+//     return arrMas.map((_, index, array)=>array[array.length-1-index])
+// }
+// const reversedArray=toReverseArr(arrMas);
+// console.log(reversedArray);
+
+// const arrMas = [1, 2, 3, 4, 5];
+// function toReverseArr(arrMas) {
+//     let newArray = [];
+//      for(let i = arrMas.length - 1; i >= 0; i--) {
+//     newArray.push(arrMas[i])
+// }
+// return newArray;
+// }
+// const reversedArray=toReverseArr(arrMas);
+// console.log(reversedArray);
+
+// 10. Дан массив с числами. Узнайте сколько элементов с начала
+// массива надо сложить, чтобы в сумме получилось больше
+// 10-ти.
+
+// const numArray = [1, 2, 3, 4, 9, 5, 6];
+// function sumNum(numArray) {
+//     let sum = 0;
+//     let effort=0;
+
+// for (i = 0; i < numArray.length; i++) {
+//     sum += numArray[i];
+//     effort = i +1;
+// }
+
+// if (sum > 10) {
+//     return effort;
+// }
+// }
+// const result = sumNum(numArray);
+// console.log(result);
+
+// 11. Напишите функцию arrayFill, которая будет заполнять
+// массив заданными значениями. Первым параметром функция
+// принимает значение, которым заполнять массив, а вторым -
+// сколько элементов должно быть в массиве. Пример:
+// arrayFill('x', 5) сделает массив ['x', 'x', 'x', 'x', 'x'].
+
+// function arrayFill(value, length) {
+//     return new Array(length).fill(value);
+// }
+// console.log(arrayFill("гусь", 7));
